732cba2da21bf5df5130d9db68ac6db3bbf8cba4
stevenvill <stevenvillarreal8@gmail.com>
2025-01-14 20:56:18 -0600

    commenting out localhost

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost"],
+    // domains: ["localhost"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
